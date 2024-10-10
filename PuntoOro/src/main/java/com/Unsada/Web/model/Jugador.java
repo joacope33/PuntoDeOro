2df5cd41c768c04e664ac1d0667f3dc91d9a3b43
emmanuel1516 <emmanuel1407@hotmail.com>
2024-10-10 01:18:28 -0300

    mdelos emma 2

--- PuntoOro/src/main/java/com/Unsada/Web/model/Jugador.java
+++ PuntoOro/src/main/java/com/Unsada/Web/model/Jugador.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -29,7 +29,7 @@ public class Jugador {
     @Column(name = "telefono")
     private String telefono;
 
-    @ManyToOne
+    @OneToMany
     @JoinColumn(name = "idcategoria")
     private Categoria categoria;
     
--- PuntoOro/src/main/java/com/Unsada/Web/model/Pareja.java
+++ PuntoOro/src/main/java/com/Unsada/Web/model/Pareja.java
@@ -1,5 +1,50 @@
 package com.Unsada.Web.model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "parejas")
+@Getter
+@Setter
 public class Pareja {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idpareja")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "idjugador1")
+    private Jugador jugador1;
+
+    @ManyToOne
+    @JoinColumn(name = "idjugador2")
+    private Jugador jugador2;
+
+    @ManyToOne
+    @JoinColumn(name = "idtorneo")
+    private Torneo torneo;
+
+    public Pareja(Jugador jugador1, Jugador jugador2, Torneo torneo) {
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+        this.torneo = torneo;
+    }
+
+    public Pareja() {
+    }
+
+    
+
+
 }
