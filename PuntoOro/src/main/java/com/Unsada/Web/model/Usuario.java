330dcc9247bb76dbb50d14b6cf2712b5133ac90e
emmanuel1516 <emmanuel1407@hotmail.com>
2024-10-10 13:07:51 -0300

    .

--- PuntoOro/src/main/java/com/Unsada/Web/model/Usuario.java
+++ PuntoOro/src/main/java/com/Unsada/Web/model/Usuario.java
@@ -1,5 +1,8 @@
 package com.Unsada.Web.model;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Usuario {
     
 }
--- PuntoOro/src/main/java/com/Unsada/Web/rest/AdministradorREST.java
+++ PuntoOro/src/main/java/com/Unsada/Web/rest/AdministradorREST.java
@@ -1,16 +1,28 @@
 package com.Unsada.Web.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Unsada.Web.model.Administrador;
 import com.Unsada.Web.service.AdministradorService;
 
+
 @RestController
-@RequestMapping("/jugadores")
+@RequestMapping("/administrador")
 public class AdministradorREST {
 
     @Autowired
     private AdministradorService administradorService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Administrador>> getAdministradoById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(administradorService.findById(id));
+    }
     
 }
