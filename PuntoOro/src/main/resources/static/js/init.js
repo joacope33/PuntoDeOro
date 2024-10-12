84114cc5b500c92d41c38774d32055c02363e31e
joacope33 <111394940+joacope33@users.noreply.github.com>
2024-10-12 19:57:02 +0000

    Se agrega funcion al archivo js de la app, se borra script de html

--- PuntoOro/src/main/resources/static/js/init.js
+++ PuntoOro/src/main/resources/static/js/init.js
@@ -1,3 +1,34 @@
 function redireccionIndex() {
     window.location.href = '/home';
   }
+
+  const modal = document.getElementById('modal');
+  const btnAbrir = document.getElementById('btn-login-abrir');
+  const btnCerrar = document.getElementById('btn-login-cerrar');
+
+  // Función para abrir el modal
+  btnAbrir.addEventListener('click', () => {
+      modal.showModal();
+  });
+
+  // Función para cerrar el modal
+  btnCerrar.addEventListener('click', () => {
+      modal.close();
+  });
+
+  // Cerrar el modal al hacer clic fuera del contenido
+  modal.addEventListener('click', (event) => {
+      if (event.target === modal) {
+          modal.close();
+      }
+  });
+
+  function actualizarCopyright() {
+    const footer = document.getElementById('Copyright');
+    const fechaActual = new Date();
+    const opciones = { year: 'numeric' };
+    footer.innerHTML = `Copyright &copy; ${fechaActual.toLocaleDateString('es-ES', opciones)} Punto de Oro`;
+}
+
+// Llamar a la función para que se ejecute al cargar la página
+document.addEventListener('DOMContentLoaded', actualizarCopyright);
